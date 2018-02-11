6320008ae5104be5f1a5be6b7a5075f6c4a80ed5
ruden <ruden91@mycelebs.com>
2018-02-11 15:04:52 +0900

    socket.io를 활용한 간단한 채팅 애플리케이션 구현

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 // app setup
 const app = express();
@@ -8,3 +9,18 @@ const server = app.listen(4000, (req, res) => {
 
 // static files
 app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('made socket connection', socket.id);
+
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  })
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  })
+});
